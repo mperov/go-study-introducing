0f2a9e32bbcbc4b2d81f9781c6b2a7dd727c0df6
Maksim Perov <coder@frtk.ru>
2020-12-07 15:47:33 +0300

    add random message

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -3,14 +3,31 @@ package greetings
 import (
     "errors"
     "fmt"
+    "math/rand"
+    "time"
 )
+
+func Init() {
+    rand.Seed(time.Now().UnixNano())
+}
+
+func randomFormat() string {
+    messages := []string {
+        "Hi, %v!",
+        "Welcome, %v!",
+        "Great to see you, %v!",
+    }
+
+    return messages[rand.Intn(len(messages))]
+}
 
 // Hello returns a greeting for the named person.
 func Hello(name string) (string, error) {
     if name == "" {
         return "", errors.New("empty new")
     }
+    Init()
     // Return a greeting that embeds the name in a message.
-    message := fmt.Sprintf("Hi, %v. Welcome!", name)
+    message := fmt.Sprintf(randomFormat(), name)
     return message, nil
 }
