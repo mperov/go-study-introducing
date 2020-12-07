00e5f1976e456eefc08cc288c9429aebc96f5bac
Maksim Perov <coder@frtk.ru>
2020-12-07 16:04:58 +0300

    add argument for programm

--- hello.go
+++ hello.go
@@ -1,18 +1,30 @@
 package main
 
 import (
+    "os"
     "fmt"
     "log"
 
     "example.com/greetings"
 )
 
+func Usage() {
+    fmt.Println("./hello <your-name>")
+}
+
 func main() {
     log.SetPrefix("greetings: ")
     log.SetFlags(0)
+    var name string = ""
+    if len(os.Args) < 2 {
+        Usage()
+        os.Exit(1)
+    } else {
+        name = os.Args[1]
+    }
 
     // Get a greeting message and print it.
-    message, err := greetings.Hello("Maksim")
+    message, err := greetings.Hello(name)
     if err != nil {
         log.Fatal(err)
     }
